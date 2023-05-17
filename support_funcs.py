101cb878f5de8c57992a7926902d8f031b1a8d4b
mosorio <matiosorio@gmail.com>
2023-05-17 13:52:29 -0300

    more changes 2 support_funcs

--- support_funcs.py
+++ support_funcs.py
@@ -71,23 +71,86 @@ def min_cuad(x,y,orden,labelX,labelY):
     
     return coef
 
-def graficar_datos(x, y, error_x, error_y, labelX, labelY):
+def graficar_datos(x, y, error_x, error_y, label_x, label_y):
     
     import matplotlib.pyplot as plt
     
     plt.figure()
-    plt.plot(x,y,'*',label='datos')
-    plt.plot(T,np.exp(coef[1])*np.exp(coef[0]*x),label='modelo obtenido')
-    plt.xlabel('T[K]',fontweight='bold',fontsize=12)
-    plt.ylabel('R [ohm] ',fontweight='bold',fontsize=12)
+    plt.plot(x,y,'*')
+    plt.xlabel(label_x,fontweight='bold',fontsize=12)
+    plt.ylabel(label_y,fontweight='bold',fontsize=12)
+    
+def graficar_datos_con_modelo(x, y, error_x, error_y, modelo_x, modelo_y, label_x, label_y):
+    
+    import matplotlib.pyplot as plt
+    
+    plt.figure()
+    plt.plot(x,y,'*',label='datos exp.')
+    plt.plot(modelo_x, modelo_y, '--', label='modelo teórico')
+    plt.xlabel(label_x,fontweight='bold',fontsize=12)
+    plt.ylabel(label_y,fontweight='bold',fontsize=12)
     plt.legend()
-    plt.title('Modelo obtenido',fontweight='bold',fontsize=14)
     
-# def graficar_datos_con_modelo(x, y, error_x, error_y, labelX, labelY, x_modelo, y_modelo):
+def estadistica_datos(datos):
     
-# def estadistica_datos():
+    import numpy as np
+    from scipy.stats import skew
     
-# def graficar_histograma():
+    # Análisis estadístico
+    print('==============================================================')
+    print('Análisis estadístico de los datos de período obtenidos')
+    print('--------------------------------------------------------------')
+    print('Cantidad de datos obtenidos: ' + str(np.size(datos)))
+    print('Promedio de los datos obtenidos [s]: ' + str(np.mean(datos)))
+    print('Desviación estándar de los datos obtenidos [s]: ' + str(np.std(datos)))
+    print('Valor mínimo: ' + str(np.min(datos)))
+    print('Valor máximo: ' + str(np.max(datos)))
+    print('Skew de la muestra: ' + str(skew(datos)))
+    print('--------------------------------------------------------------')     
+    print('Información básica del skew:')
+    print(' ')
+    print('Si el skew es nulo o muy cercano a cero, la muestra se distribuye de manera normal')
+    print(' ')
+    print('Si skew > 0, los datos están más distribuidos hacia la parte izquierda de la cola de la distribución')
+    print(' ')
+    print('Si skew < 0, los datos están más distribuidos hacia la parte derecha de la cola de la distribución')
+    print('==============================================================')
+    print(' ')
+    
+def graficar_histograma(datos, clases, label_x, label_y):
+    
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from scipy.stats import norm
+    
+    # Se grafica histograma con la frecuencia absoluta
+    counts, bins, _ = plt.hist(measurements, bins=clases, alpha=0.7, color='steelblue', edgecolor='black')
+
+    # Ancho de bin
+    bin_width = bins[1] - bins[0]
+
+    # Estadística de datos para armar Gaussiana
+    cant_medidas = len(datos)
+    mu, sigma = norm.fit(datos)
+
+    # Generate points on x-axis for Gaussian curve
+    x = np.linspace(min(datos), max(datos), 100)
+    y = norm.pdf(x, mu, std) * bin_width * total_measurements
+
+    # Normalize the Gaussian curve to match the data
+#    scaling_factor = counts.sum() / y.sum()
+ #   y *= scaling_factor
+
+    # Plot Gaussian curve
+    plt.plot(x, y, 'r-', linewidth=2)
+
+    # Set plot labels and title
+    plt.xlabel('Measurement')
+    plt.ylabel('Absolute Frequency')
+    plt.title('Histogram with Normalized Gaussian Fit')
+
+    # Display the plot
+    plt.show()
     
 # def graficar_boxplot:
 
